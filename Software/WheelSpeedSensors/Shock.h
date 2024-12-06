debc2c0fb1b2c9b6475826975cafa9a3097fa8bc
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2024-12-05 20:24:53 -0500

    Update shock.h to use inch displacement

--- Software/WheelSpeedSensors/Shock.h
+++ Software/WheelSpeedSensors/Shock.h
@@ -1,16 +1,24 @@
+const int readingAtMin = 4095;     // Reading from sensor when fully compressed
+const int readingAtMax = 0;        // Reading from sensor when fully extended
+const int readingAtCenter = 2048;  // Reading from sensor at center resting point
+
+const float inchesAtMin = -5;  // Displacement of wheels from center when fully compressed (positive)
+const float inchesAtMax = +5;  // Displacement of wheels from center when fully extended (negative)
+
+
 // Class that defines shared variables and functions between the four wheels
 class Shock {
 
 public:
 
-  int sensorPin;  // GPIO that sensor is hooked up to
-  int reading;  // analog reading value from ESP32
-  int position;  // calculated position value (0-100%) from reading
+  int sensorPin;       // GPIO that sensor is hooked up to
+  int reading;         // analog reading value from ESP32
+  float displacement;  // wheel displacement +/- from zero point in inches
 
   Shock(int pinNumber) {
     sensorPin = pinNumber;
     int reading = 0;
-    int position = 0;
+    float displacement = 0;
     pinMode(sensorPin, INPUT);
   }
 
@@ -20,10 +28,19 @@ public:
 
     reading = analogRead(sensorPin);
 
-    position = map(reading, 0, 4095, 0, 100);
+    if (reading > readingAtCenter) {
+      displacement = mapfloat(reading, readingAtCenter, readingAtMin, 0, inchesAtMin);  // We have positive displacement
+    } else {
+      displacement = mapfloat(reading, readingAtCenter, readingAtMax, 0, inchesAtMax);  // We have negative displacement
+    }
 
     Serial.print(millis());
     Serial.print(",");
-    Serial.println(position);
+    Serial.println(displacement);
   }
 };
+
+// Function for float mapping
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
